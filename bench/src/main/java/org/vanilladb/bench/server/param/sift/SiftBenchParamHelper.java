6bb5d4aad69dae08b7a17e21d98498e9fc36c63a
109090043 <i7473po@gmail>
2024-06-14 18:27:07 +0800

    update

--- bench/src/main/java/org/vanilladb/bench/server/param/sift/SiftBenchParamHelper.java
+++ bench/src/main/java/org/vanilladb/bench/server/param/sift/SiftBenchParamHelper.java
@@ -1,9 +1,8 @@
 package org.vanilladb.bench.server.param.sift;
 
 import java.util.Set;
-import java.util.logging.Logger;
+import java.util.ArrayList;
 
-import org.vanilladb.core.query.algebra.vector.NearestNeighborScan;
 import org.vanilladb.core.sql.IntegerConstant;
 import org.vanilladb.core.sql.Schema;
 import org.vanilladb.core.sql.Type;
@@ -12,13 +11,15 @@ import org.vanilladb.core.sql.storedprocedure.SpResultRecord;
 import org.vanilladb.core.sql.storedprocedure.StoredProcedureHelper;
 
 public class SiftBenchParamHelper implements StoredProcedureHelper {
-    private static Logger logger = Logger.getLogger(SiftBenchParamHelper.class.getName());
     private final String table = "sift";
     private final String embField = "i_emb";
     private VectorConstant query;
     private int numDimension;
     private Integer[] items;
     private int numNeighbors = 20; // Number of top-k
+
+    private int insertCount = 0;
+    private ArrayList<Object[]> insertHistory  = new ArrayList<>();
 
     @Override
     public void prepareParameters(Object... pars) {
@@ -29,6 +30,19 @@ public class SiftBenchParamHelper implements StoredProcedureHelper {
         }
         query = new VectorConstant(rawVector);
         items = new Integer[numNeighbors];
+
+        if (pars.length > numDimension+1){
+            insertCount = (Integer) pars[numDimension+1];
+            int ptr = numDimension + 2;
+            for (int i = 0; i < insertCount; i++){
+                Object[] insertParam = new Object[numDimension + 1];
+                for (int j = 0; j < numDimension + 1; j++) {
+                    insertParam[j] = pars[ptr + j + 1];
+                }
+                insertHistory.add(insertParam);
+                ptr = ptr + numDimension + 2;
+            }
+        }
     }
 
     @Override
@@ -45,9 +59,8 @@ public class SiftBenchParamHelper implements StoredProcedureHelper {
     public SpResultRecord newResultSetRecord() {
         SpResultRecord rec = new SpResultRecord();
         rec.setVal("rc", new IntegerConstant(numNeighbors));
+
         for (int i = 0; i < numNeighbors; i++) {
-            if (items[i]==null)
-                logger.info(i+":items:null");
             rec.setVal("id_" + i, new IntegerConstant(items[i]));
         }
         return rec;
@@ -55,11 +68,10 @@ public class SiftBenchParamHelper implements StoredProcedureHelper {
 
     @Override
     public boolean isReadOnly() {
-        return true;
+        return false;
     }
 
     public void setNearestNeighbors(Set<Integer> nearestNeighbors) {
-        // logger.info("size:"+nearestNeighbors.size());
         items = nearestNeighbors.toArray(items);
     }
 
@@ -77,5 +89,13 @@ public class SiftBenchParamHelper implements StoredProcedureHelper {
 
     public int getK() {
         return numNeighbors;
+    }
+
+    public int getInsertCount(){
+        return insertCount;
+    }
+
+    public ArrayList<Object[]> getInsertHistory() {
+        return insertHistory;
     }
 }
